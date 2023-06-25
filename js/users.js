ed62cfdda5b29f504228910bb48119b7ff5ac70a
morteza <hadafmahdi@gmail.com>
2023-06-25 16:38:18 +0330

    add users complete test inputs fix

--- js/users.js
+++ js/users.js
@@ -6,7 +6,7 @@ window.banUser = banUser
 window.addEventListener("load", () => {
     const newUserBtn = document.querySelector("#submit-btn")
 
-    // getAndShowAllUsers()
+    getAndShowAllUsers()
 
     newUserBtn.addEventListener("click", (event) => {
         event.preventDefault()
